2313ba155d334d65d61629f8074850be239241fa
Dillan Real <132624193+Th3crafting@users.noreply.github.com>
2024-11-05 17:34:42 -0500

    Proyecto final
    Primera version completa
    
    Se añade estructura y frontend

--- src/main/java/com/admondb/estudiantesregistro/dao/repository/UbicacionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.admondb.estudiantesregistro.dao.repository;
-
-import com.admondb.estudiantesregistro.model.Ubicacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UbicacionRepository extends JpaRepository<Ubicacion, Long> {
-
-}
--- src/main/java/com/admondb/estudiantesregistro/dto/UbicacionDTO.java
+++
@@ -1,9 +0,0 @@
-package com.admondb.estudiantesregistro.dto;
-
-import org.locationtech.jts.geom.Point;
-
-public record UbicacionDTO(
-        Double longitud,
-        Double latitud
-) {
-}
